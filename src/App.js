824e71c6609c524dafd4853f1ab4a60cdcc43ca9
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-03 13:00:46 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header'
 import Main from './components/main/main'
+import Footer from './components/footer/footer'
 
 
 class App extends Component {
@@ -11,11 +12,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Main />
-
-        <footer className="expanded row">
-          <h6>&copy; 2017 Erebor Industries</h6>
-          <h6 className="float-right">All rights reserved</h6>
-        </footer>
+        <Footer />
       </div>
     );
   }
